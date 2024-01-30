69f5746847536e987b507b6c5f4df332debdf9d7
silentlalit <lalit.trycoders@gmail.com>
2024-01-31 04:36:35 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ import errorHandler from "./middlewares/Error.js";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true,
   })
 );
