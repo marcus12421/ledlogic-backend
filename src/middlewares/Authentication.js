b8ac19c25a15b19ae7309f6e3560faaf396be7c3
silentlalit <lalit.trycoders@gmail.com>
2024-01-31 12:54:05 +0530

    Update Authentication.js

--- src/middlewares/Authentication.js
+++ src/middlewares/Authentication.js
@@ -5,28 +5,17 @@ import CatchAsyncError from "./CatchAsyncError.js";
 import User from "../models/UserModel.js";
 
 export const isAuthenticatedUser = async (req, res, next) => {
-  try {
-    const { token } = req.cookies;
+  const { token } = req.cookies;
 
-    if (!token) {
-      return next(
-        new ErrorHandler("Please Login to access this resource", 401)
-      );
-    }
-
-    const decodedData = jwt.verify(token, process.env.JWT_SEC_KEY);
-
-    req.user = await User.findById(decodedData.id);
-
-    next();
-  } catch (error) {
-    res.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
-
-    res.status(401).json({
-      success: false,
-      error: err.message,
-    });
+  if (!token) {
+    return next(new ErrorHandler("Please Login to access this resource", 401));
   }
+
+  const decodedData = jwt.verify(token, process.env.JWT_SEC_KEY);
+
+  req.user = await User.findById(decodedData.id);
+
+  next();
 };
 
 export const authorizeRoles = (...roles) => {
