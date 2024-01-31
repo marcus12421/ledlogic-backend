fca9484aba73d5705eaf02a1386725ff649cd3b9
silentlalit <lalit.trycoders@gmail.com>
2024-01-31 12:50:27 +0530

    load user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -46,6 +46,7 @@ export const logOut = CatchAsyncError(async (req, res, next) => {
   res.cookie("token", null, {
     expires: new Date(Date.now()),
     httpOnly: true,
+    sameSite: "none",
   });
 
   res.status(200).json({
