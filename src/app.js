a90a973f6bae820e34482a6de09480aaf481f37b
silentlalit <lalit.trycoders@gmail.com>
2024-01-31 11:44:09 +0530

    update cors

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ import errorHandler from "./middlewares/Error.js";
 const app = express();
 app.use(
   cors({
-    origin: `https://discountshippingcanada.com`,
+    origin: [`https://discountshippingcanada.com`, "http://localhost:3000"],
     credentials: true,
   })
 );
