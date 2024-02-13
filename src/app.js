99ae1913ede38701d3ea248ebecdeddd27cca5b5
silentlalit <lalit.trycoders@gmail.com>
2024-02-13 21:54:00 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,12 @@ import user from "./routes/userRoute.js";
 import errorHandler from "./middlewares/Error.js";
 
 const app = express();
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+
+  next();
+});
 app.use(
   cors({
     origin: [
